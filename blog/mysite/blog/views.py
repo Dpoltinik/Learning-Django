1ed4164584206e677e8ff4e49ec513f415ce27a1
Dmitriy <d.poltinik@yandex.ru>
2022-01-30 15:15:01 +0300

    blog application create

---
+++ blog/mysite/blog/views.py
@@ -0,0 +1,59 @@
+from shutil import ExecError
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+def post_list(request):
+    objects_list = Post.published.all()
+    paginator = Paginator(objects_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    context = {
+        'posts': posts,
+        'page': page
+    }
+    return render(request, 'blog/post/list.html', context)
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day 
+    )
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context)
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'\
+                .format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'\
+                .format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+        
+    context = {
+        'sent': sent,
+        'post': post,
+        'form': form
+    }
+
+# Create your views here.
